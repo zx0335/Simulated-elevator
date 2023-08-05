28093f78d3ac6d2b9f261f4cbc36a856857a07ba
zx0335 <121654673+zx0335@users.noreply.github.com>
2023-08-05 22:39:35 +0800

    Update lcd.h

--- include/lcd.h
+++ include/lcd.h
@@ -34,22 +34,22 @@ typedef unsigned char u8;
 typedef struct
 { // bmp图片文件头信息封装
 	// 位图文件头
-	u8 bit_file_type[2]; // 位图文件类型:'BM'->0x4d42
-	u32 file_size;		 // 整个文件大小
-	u16 reserved1;		 // 保留
-	u16 reserved2;		 // 保留
-	u32 offset;			 // 文件头到位图数据之间的偏移量
+	u8 bit_file_type[2]; 	// 位图文件类型:'BM'->0x4d42
+	u32 file_size;		// 整个文件大小
+	u16 reserved1;		// 保留
+	u16 reserved2;		// 保留
+	u32 offset;		// 文件头到位图数据之间的偏移量
 
 	// 位图信息头
 	u32 head_size;		// 位图信息头长度
-	u32 width;			// 位图宽度
-	u32 height;			// 位图高度
+	u32 width;		// 位图宽度
+	u32 height;		// 位图高度
 	u16 bit_planes;		// 位图位面数
-	u16 bits_per_pixel; // 每个像素的位数
+	u16 bits_per_pixel; 	// 每个像素的位数
 	u32 compression;	// 压缩说明
 	u32 image_size;		// 位图数据大小
-	u32 h_res;			// 水平分辨率
-	u32 v_res;			// 垂直分辨率
+	u32 h_res;		// 水平分辨率
+	u32 v_res;		// 垂直分辨率
 	u32 color_palette;	// 位图使用的颜色索引数
 	u32 vip_color;		// 重要的颜色索引数目
 } bmp_head;
@@ -62,9 +62,9 @@ public:
 	Bmp_Event &operator=(const Bmp_Event &other) = delete;
 	~Bmp_Event();
 	
-	void show_bmp(int offset_x, int offset_y, const char *bmp_name); // 显示bmp图片
-	int get_xy(int &x, int &y);										 // 获取按下的坐标
-	static Bmp_Event &getInstance();								 // 创建单例对象
+	void show_bmp(int offset_x, int offset_y, const char *bmp_name);// 显示bmp图片
+	int get_xy(int &x, int &y);					// 获取按下的坐标
+	static Bmp_Event &getInstance();				// 创建单例对象
 private:
 	int *p;
 	int lcd_fd; // 显示屏文件描述符
